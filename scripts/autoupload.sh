e704d5a9453a6f717818e51b95f4cfa7f27603e5
Tim Zimmermann <tim@linux4.de>
2021-09-11 07:01:26 +0200

    scripts: add missing -C

--- scripts/autoupload.sh
+++ scripts/autoupload.sh
@@ -15,7 +15,7 @@ DATE=$(echo $FILENAME | cut -f3 -d '-')
 RECOVERY_NAME=$(echo $FILENAME | sed 's/UNOFFICIAL/recovery/g' | sed 's/\.zip/.img/g')
 RELEASENAME="$DEVICE-$DATE"
 
-$SCRIPTDIR/otainfo.sh $ZIP $(hub browse -u) $RELEASENAME > $REPODIR/$DEVICE.json
+$SCRIPTDIR/otainfo.sh $ZIP $(hub -C $REPODIR browse -u) $RELEASENAME > $REPODIR/$DEVICE.json
 cp $BUILDDIR/recovery.img $BUILDDIR/$RECOVERY_NAME
 echo "$(sha256sum $ZIP | cut -f1 -d ' ') $FILENAME" > $REPODIR/$FILENAME.sha256
 echo "$(sha256sum $BUILDDIR/$RECOVERY_NAME | cut -f1 -d ' ') $RECOVERY_NAME" > $REPODIR/$RECOVERY_NAME.sha256
@@ -25,7 +25,7 @@ git -C $REPODIR commit -m "OTA: $DEVICE: $DATE"
 git -C $REPODIR tag "$RELEASENAME"
 git -C $REPODIR push origin HEAD:staging --tags
 
-hub -C $REPODIR release create -a $BUILDDIR/$RECOVERY_NAME -a $REPODIR/$RECOVERY_NAME.sha256 -a $ZIP -a $REPODIR/$FILENAME.sha256 -m "$DEVICE: $DATE" -t $(git rev-parse HEAD) $RELEASENAME
+hub -C $REPODIR release create -a $BUILDDIR/$RECOVERY_NAME -a $REPODIR/$RECOVERY_NAME.sha256 -a $ZIP -a $REPODIR/$FILENAME.sha256 -m "$DEVICE: $DATE" -t $(git -C $REPODIR rev-parse HEAD) $RELEASENAME
 
 git -C $REPODIR push origin HEAD:master --tags
 git -C $REPODIR push origin --delete staging
